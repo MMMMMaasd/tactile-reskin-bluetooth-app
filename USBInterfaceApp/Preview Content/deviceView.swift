8916ad7c44b974d0e6a75cbceee771e4b57e150e
MMMMMaasd <zb2253@nyu.edu>
2024-07-07 19:52:38 +0800

    Updated the app

---
+++ USBInterfaceApp/Preview Content/deviceView.swift
@@ -0,0 +1,18 @@
+//
+//  deviceView.swift
+//  USBInterfaceApp
+//
+//  Created by 卞泽宇 on 2024/6/20.
+//
+
+import SwiftUI
+
+struct deviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    deviceView()
+}
